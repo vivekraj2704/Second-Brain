1bd2025ddd26bb1b64b4ba5ccec87f46f2afacc4
vivekraj2704 <rajvivek2704@gmail.com>
2025-02-28 12:52:47 +0530

    content Schema changed

--- src/db.ts
+++ src/db.ts
@@ -6,15 +6,10 @@ const UserSchema = new Schema({
     password: String
 })
 
-enum contentEnum {
-    "Politics",
-    "Sports"
-}
 export const User = mongoose.model('User', UserSchema);
 
 const ContentSchema = new Schema({
     link: String,
-    type: String, enum: contentEnum,
     title: String,
     tags: [{type: mongoose.Types.ObjectId, ref: 'Tag'}],
     UserId: {type: Schema.Types.ObjectId, ref: 'User', required: true}
