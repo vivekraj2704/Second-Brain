fcdf3a1f972dd9294fd2b2d06c9d05084101c4a1
vivekraj2704 <rajvivek2704@gmail.com>
2025-03-01 14:59:03 +0530

    Sidebar Init

---
+++ frontend/src/components/ui/SidebarItem.tsx
@@ -0,0 +1,12 @@
+import { ReactElement } from "react"
+
+export function SidebarItem({text, icon}: {
+    text: string,
+    icon: ReactElement
+}) {
+    return(
+        <div className="flex">
+            {icon} {text}
+        </div>
+    )
+}
