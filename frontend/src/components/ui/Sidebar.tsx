f4c2cd13954f5e4786d39505a4e411fdfc2acf2b
vivekraj2704 <rajvivek2704@gmail.com>
2025-03-01 18:10:21 +0530

    almost done with sidebar

--- frontend/src/components/ui/Sidebar.tsx
+++ frontend/src/components/ui/Sidebar.tsx
@@ -1,7 +1,21 @@
+import { Logo } from "../../icons/Logo";
+import { TwitterIcon } from "../../icons/TwitterIcon";
+import { YoutubeIcon } from "../../icons/YoutubeIcon";
+import { SidebarItem } from "./SidebarItem";
+
 export function Sidebar() {
     return(
-        <div className="h-screen bg-white border-r w-72 fixed left-0 top-0">
-
+        <div className="h-screen bg-white border-r w-72 fixed left-0 top-0 pl-6">
+            <div className="flex text-2xl pt-8 items-center">
+                <div className="pr-2 text-purple-400">
+                    <Logo />
+                </div>
+                Second Brain
+            </div>
+            <div className="pt-8 pl-4">
+                <SidebarItem text="Twitter" icon={<TwitterIcon />}/>
+                <SidebarItem text="YouTube" icon={<YoutubeIcon />}/>
+            </div>
         </div>
     )
 }
--- frontend/src/components/ui/SidebarItem.tsx
+++ frontend/src/components/ui/SidebarItem.tsx
@@ -5,8 +5,13 @@ export function SidebarItem({text, icon}: {
     icon: ReactElement
 }) {
     return(
-        <div className="flex">
-            {icon} {text}
+        <div className="flex text-gray-700 py-2 cursor-pointer hover:bg-gray-200 rounded max-w-48 pl-4 transition-all duration-150">
+            <div className="pr-2">
+                {icon} 
+            </div>
+            <div>
+                {text}
+            </div>
         </div>
     )
 }
