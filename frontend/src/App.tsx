656d887131e69dd5042bfddc30b1396616711b41
vivekraj2704 <rajvivek2704@gmail.com>
2025-03-01 13:09:28 +0530

    added cardComponent

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,22 @@
 import './index.css'
 import { Button } from './components/ui/Button'
-import { Plusicon } from './icons/Plusicon'
 import { Shareicon } from './icons/Shareicon'
+import { Card } from './components/ui/Card'
 
 function App() {
 
   return (
-    <>
-      <Button startIcon={<Plusicon svgsize="md"/>} text="Add new Item mid" variant='primary' size='sm' onClick={() => console.log('ok')}/>
-      <Button startIcon={<Shareicon svgsize="md"/>} text="Add new Item mid" variant='primary' size='md' onClick={() => console.log('ok')}/>
-      <Button startIcon={<Shareicon svgsize="md"/>} text="Add new Item mid" variant='primary' size='lg' onClick={() => console.log('ok')}/>
-      <Button startIcon={<Shareicon svgsize='sm'/>} text="Add new Item small" variant='primary' size='sm' onClick={() => console.log('ok')}/>
-      <Button startIcon={<Shareicon svgsize='sm'/>} text="Add new Item small" variant='primary' size='md' onClick={() => console.log('ok')}/>
-      <Button startIcon={<Shareicon svgsize='sm'/>} text="Add new Item small" variant='primary' size='lg' onClick={() => console.log('ok')}/>
-      <Button startIcon={<Shareicon svgsize='lg'/>} text="Add new Item large" variant='primary' size='sm' onClick={() => console.log('ok')}/>
-      <Button startIcon={<Shareicon svgsize='lg'/>} text="Add new Item large" variant='primary' size='md' onClick={() => console.log('ok')}/>
-      <Button startIcon={<Shareicon svgsize='lg'/>} text="Add new Item large" variant='primary' size='lg' onClick={() => console.log('ok')}/>
-    </>
+    <div className='p-4'>
+      <div className='flex justify-end gap-2'>
+        <Button startIcon={<Shareicon svgsize="md"/>} text="Add new Item mid" variant='primary' size='md' onClick={() => console.log('ok')}/>
+        <Button startIcon={<Shareicon svgsize="md"/>} text="Add new Item mid" variant='primary' size='md' onClick={() => console.log('ok')}/>
+      </div>
+
+      <div className='flex gap-4'>
+        <Card type={"twitter"} link={"https://x.com/charliekirk11/status/1895532670521917571"} title='TRUMP!!!!!'/>
+        <Card type={"youtube"} link={"https://www.youtube.com/watch?v=SCcgh2pXUg0"} title={"Maniac"}/>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/components/ui/Card.tsx
+++ frontend/src/components/ui/Card.tsx
@@ -1,14 +1,43 @@
-import { ReactElement } from "react"
+import { Shareicon } from "../../icons/Shareicon";
 
-interface CardInterface{
-    svg: ReactElement,
-    text: string,
+interface CardProps{
+    title: string;
+    link: string;
+    type: "youtube" | "twitter";
 }
 
-export function Card(props: CardInterface) {
+export function Card({ title, link, type}: CardProps) {
     return(
-        <>
-            
-        </>
+        <div className="bg-white p-4 rounded-md border border-gray-200 max-w-72 min-h-48 min-w-72">
+            <div className="flex justify-between">
+                <div className="flex items-center text-md">
+                    <div className="text-gray-500 pr-2">
+                        <Shareicon />
+                    </div>
+                    {title}
+                </div>
+                <div className="flex items-center">
+                    <div className="pr-2 text-gray-500">
+                        <a href={link} target="_blank">
+                            <Shareicon />
+                        </a>
+                    </div>
+                    <div className="text-gray-500">
+                        <Shareicon />
+                    </div>
+                </div>
+            </div>
+
+            <div className="pt-4">
+                {type === "youtube" && 
+                <iframe className="w-full" src={link.replace("watch", "embed").replace("?v=", "/")} title="MANIAC (Official Video): YO YO HONEY SINGH | ESHA GUPTA | GLORY | BHUSHAN KUMAR" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>}
+
+                {type === "twitter" && 
+                <blockquote className="twitter-tweet">
+                <a href={link.replace("x.com", "twitter.com")}></a> 
+                </blockquote>
+                }   
+            </div>
+        </div>
     )
 }
