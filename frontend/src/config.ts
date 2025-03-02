19d4b308cf7b1608efea1d6d6cd75364078d49c0
vivekraj2704 <rajvivek2704@gmail.com>
2025-03-02 12:27:23 +0530

    fixed the backend url issue

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,1 +1,3 @@
-export const BACKEND_URL = "http://localhost:3000"
+const BEURL = import.meta.env.VITE_BACKEND_URL;
+// export const BACKEND_URL = "http://localhost:3000"
+export const BACKEND_URL = BEURL
